9775ed9675abf51e60553022255863a026054152
Chocoball9 <52275589+Chocoball9@users.noreply.github.com>
2019-06-27 15:15:32 +0700

    Test

--- app.py
+++ app.py
@@ -18,9 +18,86 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_channel_access_token = 'FT9zaPscYQJbAQtWq4bHl1iM4Kpx7tHthvQZn4KcCj4eta1Nfq0T+jBpYxGtxPtrRrj3/GD6QLDCQxThjS52itajTMpnvsnJzRwuMj9xivIQIN1fkIEslMimIRNnyXF/heI7MCcCtpCNcbD2Y4EDewdB04t89/1O/w1cDnyilFU='
+line_channel_access_token = '5pbB04oQu/OoOnX1uNRNwHWZ7UqCU3QoMPfDYSIC0PM5UltsMZzRFCkLFNkChxIO7RnLydCjX+vWmhmaS+A2LfL21qxYjNHnNsJMcH499zZLm6Jp/N4QNwfvS9W5p6t4T1UrXZe7/6dx+SsFrggvBQdB04t89/1O/w1cDnyilFU='
+# line_bot_api = LineBotApi(line_channel_access_token)
+# Authorization = "Bearer {}".format(line_channel_access_token)
+#
+#
+# @app.route("/callback", methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
+#
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+#
+#     body = json.loads(body)
+#     print (body)
+#
+#     return '',200
+#
+# if __name__ == "__main__":
+#     app.run()
+
+###############################################
+# ไว้ใส่ข้อความ
+
+# line_bot_api = LineBotApi(line_channel_access_token)
+# Authorization = "Bearer {}".format(line_channel_access_token)
+#
+#
+# @app.route("/callback", methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
+#
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+#
+#     body = json.loads(body)
+#     print (body)
+#
+#     reply_token = body['events'][0]["replyToken"]
+#     print("reply_token: {}".format(reply_token))
+#
+#     event_type = body['events'][0]['type']
+#     print("event_type: {}".format(event_type))
+#
+#     if event_type == "message":
+#         message_type = body['events'][0]['message']['type']
+#         # print("message_type: {}".format(message_type))
+#         if message_type == "text":
+#             text = body['events'][0]['message']['text']
+#             print("text: {}".format(text))
+########             if "home" in text or "Home" in text:       แก้ input ตรงนี้
+#                 print("replying text:{}".format(text))
+#                 reply_menu(reply_token)
+#
+# def reply_menu(reply_token):
+#     response = requests.post(
+#         url="https://api.line.me/v2/bot/message/reply",
+#         headers={
+#             "Content-Type": "application/json",
+#             "Authorization": Authorization,
+#         },
+#         data=json.dumps({
+#             "replyToken": str(reply_token),
+########             "messages": [ใส่ตรงนี้]
+#         })
+#     )
+#
+#
+#
+# if __name__ == "__main__":
+#     app.run()
+
+
+
+
+
 line_bot_api = LineBotApi(line_channel_access_token)
-handler = WebhookHandler('96363b03e615241b7d68c6ef2ba8dcf2')
 Authorization = "Bearer {}".format(line_channel_access_token)
 
 
@@ -48,7 +125,10 @@ def callback():
         if message_type == "text":
             text = body['events'][0]['message']['text']
             print("text: {}".format(text))
-            if "home" in text or "Home" in text:
+            # if "home" in text or "Home" in text:
+            #     print("replying text:{}".format(text))
+            #     reply_menu(reply_token)
+                if "home" in text or "Home" in text:
                 print("replying text:{}".format(text))
                 reply_menu3(reply_token)
             elif text == "weather":
@@ -58,120 +138,7 @@ def callback():
 
     return '',200
 
-
-
 def reply_menu(reply_token):
-    print('from reply menu')
-    print("Authorization:{}".format(Authorization))
-    print("reply_token:{}".format((reply_token)))
-
-    # line_bot_api.reply_message(reply_token, TextSendMessage(text='Hello World222!'))
-
-    response = requests.post(
-        url="https://api.line.me/v2/bot/message/reply",
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": Authorization,
-        },
-        data=json.dumps({
-            "replyToken": str(reply_token),
-            "messages": [
-                {
-                    "type": "text",
-                    "text": "เชิญเลือกเมนูได้เลยครับ",
-                    "quickReply": {
-                        "items": [
-                            {
-                                "type": "action",
-                                "imageUrl": 'https://drive.google.com/uc?export=download&id=1Hsd2w0YF1ThUIQEiXXm-6s67fmV7azh2',
-                                "action": {
-                                    "type": "message",
-                                    "label": "home",
-                                    "text": "Home"
-                                }
-                            },
-                            {
-                                "type": "action",
-                                "imageUrl": 'https://drive.google.com/uc?export=download&id=1Hsd2w0YF1ThUIQEiXXm-6s67fmV7azh2',
-                                "action": {
-                                    "type": "message",
-                                    "label": "weather",
-                                    "text": "weather"
-                                }
-                            },
-                            {
-                                "type": "action",
-                                "imageUrl": 'https://drive.google.com/uc?export=download&id=1Hsd2w0YF1ThUIQEiXXm-6s67fmV7azh2',
-                                "action": {
-                                    "type": "message",
-                                    "label": "energy",
-                                    "text": "energy"
-                                }
-                            }
-                        ]
-                    }
-                }
-            ]
-        })
-    )
-    print('Response HTTP Status Code: {status_code}'.format(
-        status_code=response.status_code))
-    print('Response HTTP Response Body: {content}'.format(
-        content=response.content))
-
-
-def reply_menu2(reply_token):
-    print('from reply menu')
-    print("Authorization:{}".format(Authorization))
-    print("reply_token:{}".format((reply_token)))
-
-    # line_bot_api.reply_message(reply_token, TextSendMessage(text='Hello World222!'))
-
-    response = requests.post(
-        url="https://api.line.me/v2/bot/message/reply",
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": Authorization,
-        },
-        data=json.dumps({
-            "replyToken": str(reply_token),
-            "messages": [{
-  "type": "template",
-  "altText": "this is a carousel template",
-  "template": {
-    "type": "carousel",
-    "actions": [],
-    "columns": [
-      {
-        "thumbnailImageUrl": "https://d3n8a8pro7vhmx.cloudfront.net/edonsw/pages/995/attachments/original/1386210667/green_energy_320.jpg",
-        "text": "Energy",
-        "actions": [
-          {
-            "type": "message",
-            "label": "click",
-            "text": "energy"
-          }
-        ]
-      },
-      {
-        "thumbnailImageUrl": "https://d3n8a8pro7vhmx.cloudfront.net/edonsw/pages/995/attachments/original/1386210667/green_energy_320.jpg",
-        "text": "Weather",
-        "actions": [
-          {
-            "type": "message",
-            "label": "click",
-            "text": "weather"
-          }
-        ]
-      }
-    ]
-  }
-}]
-        })
-    )
-
-
-def reply_menu3(reply_token):
     response = requests.post(
         url="https://api.line.me/v2/bot/message/reply",
         headers={
@@ -212,50 +179,6 @@ def reply_menu3(reply_token):
     ]
   }
 }]
-        })
-    )
-
-
-
-
-def reply_menu4(reply_token):
-    response = requests.post(
-        url="https://api.line.me/v2/bot/message/reply",
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": Authorization,
-        },
-        data=json.dumps({
-            "replyToken": str(reply_token),
-            "messages": []
-        })
-    )
-
-
-def reply_menu5(reply_token):
-    response = requests.post(
-        url="https://api.line.me/v2/bot/message/reply",
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": Authorization,
-        },
-        data=json.dumps({
-            "replyToken": str(reply_token),
-            "messages": []
-        })
-    )
-
-
-def reply_menu6(reply_token):
-    response = requests.post(
-        url="https://api.line.me/v2/bot/message/reply",
-        headers={
-            "Content-Type": "application/json",
-            "Authorization": Authorization,
-        },
-        data=json.dumps({
-            "replyToken": str(reply_token),
-            "messages": []
         })
     )
 
